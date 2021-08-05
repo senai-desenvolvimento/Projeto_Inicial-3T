754bdcdb0cc84baa8d9833375ce18a24340c198a
DEV <dev@info.local>
2021-08-05 17:11:25 -0300

    frontend no senai

--- FRONT-END/WEB/inicial-ui/src/pages/Salas/Salas.jsx
+++ FRONT-END/WEB/inicial-ui/src/pages/Salas/Salas.jsx
@@ -8,28 +8,39 @@ import {Link} from 'react-router-dom';
 
 // Components
 import MenuControle from '../../components/MenuControle';
+import Modal from '../../components/Modal';
 
 // Styles
 import '../../assets/css/salas.css';
 import '../../assets/css/reset.css';
+import '../../assets/css/modal.css';
+
+// Imgs
+import icon from '../../assets/img/cards-lista-btn.svg';
 
 
 class Salas extends Component {
     constructor(props){
         super(props);
         this.state = {
-            x : ''
+            isModalOpenCadastro : false
         }
     }
 
+    
     render() {
+        document.getElementById("modal").addEventListener("click", teste);
+        function teste() {
+            this.setState({isModalOpenCadastro : false})
+        }
+        
         return(
             <>
                 <MenuControle />
                 <div className="salas-background">
                     <div className="salas-btns">
                         <div className="salas-btns-cadastro">
-                            <button>Cadastrar Sala</button>
+                            <button onClick={() => this.setState({isModalOpenCadastro : true})}>Cadastrar Sala</button>
                         </div>
 
                         <div className="salas-btns-ordenar">
@@ -40,47 +51,199 @@ class Salas extends Component {
                     <div className="salas-lista">
 
                         <div className="salas-lista-card">
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
 
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
+
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                         <div className="salas-lista-card">
-                            
-                        </div>
+                            <button className="salas-card-click">
+                                <div className="salas-card-btn-lateral">
+                                    <img draggable="false" src={icon} />
+                                </div>
 
-                        <div className="salas-lista-card">
-                            
-                        </div>
-
-                        <div className="salas-lista-card">
-                            
+                                <div className="salas-card-text">
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Sala</span>
+                                        <p className="salas-card-text-content">Informática</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Andar</span>
+                                        <p className="salas-card-text-content">Térreo</p>
+                                    </div>
+                                    <div className="salas-card-text-item">
+                                        <span className="salas-card-text-title">Metragem</span>
+                                        <p className="salas-card-text-content">14m²</p>
+                                    </div>
+                                </div>
+                            </button>
                         </div>
 
                     </div>
                 </div>
+
+                <Modal isOpen={this.state.isModalOpenCadastro}>
+                    <div className="modal" id="modal" onClick={() => this.setState({isModalOpenCadastro : false})}>
+                        <div className="modaldasda">
+
+                        </div>
+                    </div>
+                </Modal>
             </>
         )
     }
